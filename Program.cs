73047791e8bf5c1b70368a86bffab82c49794a06
Nuno Amaral <n20amaral@outlook.com>
2022-12-01 13:21:11 +0000

    Day 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine($"Day 1 : {Day1()}");
+
+
+static int Day1()
+{
+    var current = 0;
+    var max = 0;
+    foreach (string line in System.IO.File.ReadLines(@"inputs/1.txt"))
+    {
+        if (String.IsNullOrWhiteSpace(line))
+        {
+            max = max > current ? max : current;
+            current = 0;
+            continue;
+        }
+        var value = 0;
+        Int32.TryParse(line, out value);
+        current += value;
+    }
+
+    return max;
+}
