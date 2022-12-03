17cd7564abe847385ad529b735e346d4dafcb926
Nuno Amaral <n20amaral@outlook.com>
2022-12-03 02:43:41 +0000

    added day3 part1 solution

--- AoC22/Solutions/Day3.cs
+++ AoC22/Solutions/Day3.cs
@@ -4,6 +4,57 @@ public static class Day3
 {
     public static (string, string) GetAnswers(string inputFilePath)
     {
-        return (String.Empty, String.Empty);
+        var rucksacks = LoadRuckSacks(inputFilePath);
+
+        var part1 = rucksacks.Sum(r => r.SharedPriority);
+
+        return (part1.ToString(), String.Empty);
+    }
+
+    private static IEnumerable<RuckSack> LoadRuckSacks(string filePath)
+    {
+        var rucksacks = new List<RuckSack>();
+
+        foreach (var line in System.IO.File.ReadLines(filePath))
+        {
+            rucksacks.Add(new RuckSack(line));
+        }
+
+        return rucksacks;
+    }
+
+    private class RuckSack
+    {
+        private string _leftCompartiment;
+        private string _rightCompartiment;
+        public RuckSack(string content)
+        {
+            (_leftCompartiment, _rightCompartiment) = SplitContent(content);
+        }
+
+        public int SharedPriority => GetPriority(GetCompartimentIntersection().First());
+
+        private int GetPriority(char itemType)
+        {
+            var charCode = (int)itemType;
+            var firstLower = (int)'a';
+            var firstUpper = (int)'A';
+
+            return charCode >= firstLower ? charCode - firstLower + 1 : charCode - firstUpper + 27;
+        }
+
+        private IEnumerable<char> GetCompartimentIntersection()
+        {
+            return _leftCompartiment.ToCharArray().Intersect(_rightCompartiment.ToCharArray());
+        }
+
+        private (string, string) SplitContent(string content)
+        {
+            var half = content.Length / 2;
+            var left = content.Substring(0, half);
+            var right = content.Substring(half, half);
+
+            return (left, right);
+        }
     }
 }
