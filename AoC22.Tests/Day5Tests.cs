d741a1ad6efaa7b0a971bb5495c509a790cda0b2
Nuno Amaral <n20amaral@outlook.com>
2022-12-05 12:57:33 +0000

    added day5 part1 solution

--- AoC22.Tests/Day5Tests.cs
+++ AoC22.Tests/Day5Tests.cs
@@ -7,7 +7,7 @@ namespace Tests;
 public class Day5Tests
 {
     [Theory]
-    [InlineData("InputMocks/4.txt")]
+    [InlineData("InputMocks/5.txt")]
     public void Part1(string input)
     {
         var (part1, part2) = Day5.GetAnswers(input);
--- AoC22/Solutions/Day5.cs
+++ AoC22/Solutions/Day5.cs
@@ -4,6 +4,100 @@ public static class Day5
 {
     public static (string, string) GetAnswers(string inputFilePath)
     {
-        return (String.Empty, String.Empty);
+        var ship = LoadShipFromFile(inputFilePath);
+
+        var part1 = ship.TopContainers();
+
+        return (string.Concat(part1), String.Empty);
+    }
+
+    private static Ship LoadShipFromFile(string filePath)
+    {
+        var containersInfo = new List<string>();
+        Ship ship = null;
+        foreach (var line in File.ReadLines(filePath))
+        {
+            if (ship == null)
+            {
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    int numberOfStacks = containersInfo.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Count();
+                    var containers = ExtractContainers(numberOfStacks, containersInfo.SkipLast(1));
+
+                    ship = new Ship(numberOfStacks, containers);
+                    continue;
+                }
+
+                containersInfo.Add(line);
+                continue;
+            }
+
+            var instructions = new string[] { "move", "from", "to" };
+            var instructionValues = line.Split(instructions, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(v => int.Parse(v)).ToArray();
+
+            ship.RunInstruction(instructionValues[0], instructionValues[1], instructionValues[2]);
+        }
+
+        return ship ?? new Ship(0);
+    }
+
+    private static IEnumerable<char[]> ExtractContainers(int numberOfStacks, IEnumerable<string> containerInfo)
+    {
+        var containers = new List<char[]>();
+
+        foreach (var line in containerInfo)
+        {
+            var containersRow = line.Chunk(4).Select(c => c[1]).ToArray();
+            containers.Add(containersRow);
+        }
+
+        containers.Reverse();
+
+        return containers;
+    }
+
+    private class Ship
+    {
+        private Stack<char>[] _containerStacks;
+
+        public Ship(int numberOfStacks)
+        {
+            _containerStacks = new Stack<char>[numberOfStacks];
+
+            for (int i = 0; i < numberOfStacks; i++)
+            {
+                _containerStacks[i] = (new Stack<char>());
+            }
+        }
+        public Ship(int numberOfStacks, IEnumerable<char[]> containers) : this(numberOfStacks)
+        {
+            foreach (var containerRow in containers)
+            {
+                for (int i = 0; i < numberOfStacks; i++)
+                {
+                    if (containerRow[i] == ' ')
+                    {
+                        continue;
+                    }
+                    _containerStacks[i].Push(containerRow[i]);
+                }
+            }
+        }
+
+        public IEnumerable<char> TopContainers()
+        {
+            return _containerStacks.Select(s => s.Peek()).ToArray();
+        }
+
+        public void RunInstruction(int quantity, int from, int to)
+        {
+            var fromContainer = _containerStacks.ElementAt(from - 1);
+            var toContainer = _containerStacks.ElementAt(to - 1);
+
+            for (int i = 0; i < quantity; i++)
+            {
+                toContainer.Push(fromContainer.Pop());
+            }
+        }
     }
 }
